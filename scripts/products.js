8b66f1707f7940c1572165e58b04164488d9fa0c
Tatiana Potapova <tpotapova@my.bcit.ca>
2021-04-15 16:53:32 -0700

    small changes

--- scripts/products.js
+++ scripts/products.js
@@ -123,7 +123,7 @@ function productsQuery() {
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
                         </div>
                         <small class="text-muted">9 mins</small>
                         </div>
@@ -174,7 +174,7 @@ function highestPrice() {
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
                         </div>
                         <small class="text-muted">9 mins</small>
                         </div>
@@ -227,7 +227,7 @@ function productsQueryReview() {
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                            <button type="button" class="btn btn-sm btn-outline-secondary">Buy</button>
                         </div>
                         <small class="text-muted">9 mins</small>
                         </div>
